4a47891071276fb9ffee2611e85e50e3613572f0
CrumrineCoder <crumrinecoding@gmail.com>
2019-04-02 09:44:32 -0400

    header and center and message lefthand

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import logo from '../assets/logo-sf-small.png';
 class Header extends Component {
   render() {
     return (
-      <div className="Header">
+      <div className="header">
         <img className="headerLogo" src={logo} />
-        <h4 className="headerMessageViewer">Message Viewer</h4>
+        <p className="headerMessageViewer">Message Viewer</p>
       </div>
     );
   }
--- src/components/Message.js
+++ src/components/Message.js
@@ -8,8 +8,8 @@ class Message extends Component {
     return (
       <div className="message">
         <div className="messageLeftHandProfileContent">
-            <img src={content.avatar} />
-            <p> {content.handle} </p>
+            <img src={content.avatar} className="messageLeftHandProfileContentImage"/>
+            <p className="messageLeftHandProfileContentHandle"> {content.handle} </p>
         </div>
         <p> {content.content} </p>
       </div>
